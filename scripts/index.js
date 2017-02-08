7b95a525a573245a2b5799dcfa9813061d8ae0ae
neelesh <neelesh@server2.com>
2017-02-08 21:16:57 +0530

    Adding libs : jquery, dojo and bootstrap.

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
 /**
  * Created by neelesh on 8/2/17.
  */
+define([
+    "dojo/_base/declare",
+    "dijit/_WidgetBase",
+    "dijit/_Container",
+    "dojo/domReady!"
+], function (declare, _WidgetBase, _Container) {
+    return declare([_WidgetBase, _Container], {
+        postCreate: function () {
+            console.log("In post create.");
+            this.placeAt(document.body);
+        }
+    });
+});
